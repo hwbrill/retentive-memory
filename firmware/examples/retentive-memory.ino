dc563e949f254fe1295f45213c5096e2838968dc
Herb Brill <hwbrill@users.noreply.github.com>
2017-01-16 13:38:39 -0600

    Update retentive-memory.ino

--- firmware/examples/retentive-memory.ino
+++ firmware/examples/retentive-memory.ino
@@ -1,1 +1,37 @@
+int led = D7;  // The on-board LED
+int oneshot = 0;
 
+void setup() {
+
+  pinMode(led, OUTPUT);
+  
+  Particle.function("SP", text);
+  
+  Particle.function("led",ledToggle);
+  
+}
+
+void loop() {
+
+}
+
+int text(String array)
+{
+	Particle.publish("SP-return", array);
+	return 1;	
+}	
+
+int ledToggle(String command)
+{
+	if (command == "on") {
+		digitalWrite(led,HIGH);
+		return 1;
+	}
+	else if (command == "off") {
+		digitalWrite(led,LOW);
+		return 0;
+	}
+	else {
+		return -1;
+	}
+}
